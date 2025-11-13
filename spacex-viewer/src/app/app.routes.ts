b92cbf67dd2487ff8783eb55a4ed10ba12b58408
Zhan1bek <zhanibekmyrzakhan@gmail.com>
2025-11-14 04:13:08 +0500

    created other 3 pages

--- spacex-viewer/src/app/app.routes.ts
+++ spacex-viewer/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home';
+import { AboutComponent } from './pages/about/about';
+import { LoginComponent } from './pages/login/login';
+
 import { LaunchList } from './components/launch-list/launch-list';
 import { LaunchDetail } from './components/launch-detail/launch-detail';
 
 export const routes: Routes = [
-  { path: '', component: LaunchList },
-  { path: 'launch/:id', component: LaunchDetail },
-  {path: '**', redirectTo: ''}
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'items', component: LaunchList },
+  { path: 'items/:id', component: LaunchDetail },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
 ];
