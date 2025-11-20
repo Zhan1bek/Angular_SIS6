722dbe8f016575bf532ed190615cab2bf2fcd8e3
Zhan1bek <zhanibekmyrzakhan@gmail.com>
2025-11-21 00:46:36 +0500

    env

---
+++ spacex-viewer/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBTvVUVnq9XqkKMLRoUIkhDzHlJsZrHB_E",
+    authDomain: "spacex-c31c1.firebaseapp.com",
+    projectId: "spacex-c31c1",
+    storageBucket: "spacex-c31c1.firebasestorage.app",
+    messagingSenderId: "238180389858",
+    appId: "1:238180389858:web:51dfcfe63aa78cd0d49fbb",
+    measurementId: "G-45M6E35DH8"
+  }
+};
